53e62eb1739086ac17645655bfbd361fe7b17e86
John Carlo Lipa <johncarlolipa@gmail.com>
2024-03-05 19:29:50 +0800

    done test

--- problems.js
+++ problems.js
@@ -12,16 +12,16 @@ function plusFive(num) {
 }
 
 const plusTen = (num) => num + 10;
-console.log(plusTen(7));
+console.log(plusTen(10));
 
 /*
-    Original:
-    Write a function `printFives(max)` that prints out the multiples of 5 that
-    are less than max.
+  Original:
+  Write a function `printFives(max)` that prints out the multiples of 5 that
+  are less than max.
 
-    New:
-    Write a function `returnSevens(max)` that returns an array that contains
-    multiples of 7 that are less than max.
+  New:
+  Write a function `returnSevens(max)` that returns an array that contains
+  multiples of 7 that are less than max.
 */
 function printFives(max) {
   for (let i = 0; i < max; i++) {
@@ -31,84 +31,86 @@ function printFives(max) {
   }
 }
 
-const returnSevens = (max) =>
-  Array.from({ length: max / 7 }, (_, index) => index * 7);
+const returnSevens = (max) => {
+  for (let i = 0; i < max; i++) {
+    if (i % 7 === 0) {
+      console.log(i);
+    }
+  }
+};
 
-const result = returnSevens(21);
-console.log(result);
+console.log(returnSevens(20));
 
 /*
-    Original:
-    Write a function named `eitherStringIncluded(sentence, word1, word2)` that
-    accepts a sentence and two words as arguments. The `eitherStringIncluded`
-    function should return `true` if *either* `word1` or `word2` is found in
-    the sentence, and `false` if neither is found.
+  Original:
+  Write a function named `eitherStringIncluded(sentence, word1, word2)` that
+  accepts a sentence and two words as arguments. The `eitherStringIncluded`
+  function should return `true` if *either* `word1` or `word2` is found in
+  the sentence, and `false` if neither is found.
 
-    New:
-    Write a function named `bothStringsIncluded(sentence, word1, word2)` that
-    accepts a sentence and two words as arguments. The `bothStringsIncluded`
-    function should return `true` if *both* `word1` and `word2` are found in
-    the sentence, and `false` if neither or only 1 is found.
+  New:
+  Write a function named `bothStringsIncluded(sentence, word1, word2)` that
+  accepts a sentence and two words as arguments. The `bothStringsIncluded`
+  function should return `true` if *both* `word1` and `word2` are found in
+  the sentence, and `false` if neither or only 1 is found.
 */
 function eitherStringIncluded(sentence, word1, word2) {
   return sentence.includes(word1) || sentence.includes(word2);
 }
 
 const bothStringsIncluded = (sentence, word1, word2) => {
-  return sentence.includes(word1) && sentence.includes(word2);
+  return (
+    sentence.toLowerCase().includes(word1.toLowerCase()) &&
+    sentence.toLowerCase().includes(word2.toLowerCase())
+  );
 };
-/*
-    Original:
-    Write a function `sumArray(arr)` that takes in an array of numbers and
-    returns the total sum of all the numbers.
 
-    New:
-    Write a function `productArray(arr)` that takes in an array of numbers and
-    returns the product of all the numbers. The product of an array  is the number
-    you get when you multiply all the numbers together.
+console.log(bothStringsIncluded("Love me or hate me", "love", "hate"));
+/*
+  Original:
+  Write a function `sumArray(arr)` that takes in an array of numbers and
+  returns the total sum of all the numbers.
+
+  New:
+  Write a function `productArray(arr)` that takes in an array of numbers and
+  returns the product of all the numbers. The product of an array  is the number
+  you get when you multiply all the numbers together.
 */
 function sumArray(arr) {
-  let sum = [];
+  let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return sum;
 }
 
-const productArray = (arr) => {
-  let product = 1;
-  for (let i = 0; i < arr.length; i++) {
-    product *= arr[i];
-  }
-  return product;
-};
+const productArray = (arr) => arr.reduce((a, b) => a * b, 1);
+console.log(productArray([3, 4, 5]));
 
-console.log(productArray([1, 2, 3, 4]));
 /*
-    Original:
-    Write a function `threeOrSeven` that takes in a number and returns `true`
-    if the number is divisible by either 3 or 7 and `false` otherwise.
+  Original:
+  Write a function `threeOrSeven` that takes in a number and returns `true`
+  if the number is divisible by either 3 or 7 and `false` otherwise.
 
-    New:
-    Write a function `fiveAndEleven` that takes in a number and returns `true`
-    if the number is divisible by BOTH 5 and 11 and `false` otherwise.
+  New:
+  Write a function `fiveAndEleven` that takes in a number and returns `true`
+  if the number is divisible by BOTH 5 and 11 and `false` otherwise.
 */
 function threeOrSeven(num) {
   return num % 3 === 0 || num % 7 === 0;
 }
 
-const fiveAndEleven = (num) => {
-  return num / 5 === 0 && num / 11 === 0 ? true : false;
-};
+const fiveAndEleven = (num) => num % 5 === 0 && num % 11 === 0;
+console.log(fiveAndEleven(21));
 
 /*
-    Original:
-    Write a function, `countVowels(word)`, that takes in a string word and
-    returns the number of vowels in the word.
+  Original:
+  Write a function, `countVowels(word)`, that takes in a string word and
+  returns the number of vowels in the word.
 
-    New:
-    Write a function, `countConsonants(word)`, that takes in a string word and
-    returns the number of consonants in the word.
+  New:
+  Write a function, `countConsonants(word)`, that takes in a string word and
+  returns the number of consonants in the word.
 */
 function countVowels(word) {
   const vowels = ["a", "e", "i", "o", "u"];
@@ -122,13 +124,29 @@ function countVowels(word) {
 }
 
 const countConsonants = (word) => {
-  const vowels = ["a", "e", "i", "o", "u"];
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-  const consonants = Array.from(alphabet).filter(
-    (letter) => !vowels.includes(letter)
-  );
-
+  const consonants = [
+    "b",
+    "c",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
   let count = 0;
   for (let i = 0; i < word.length; i++) {
     if (consonants.includes(word[i])) {
@@ -141,39 +159,39 @@ const countConsonants = (word) => {
 console.log(countConsonants("make"));
 
 /*
-    Original:
-    Write a function `whisper` that takes in a string and returns a "whispered"
-    version of that string. Use the `.toLowerCase()` function on a string to
-    see what it does!
+  Original:
+  Write a function `whisper` that takes in a string and returns a "whispered"
+  version of that string. Use the `.toLowerCase()` function on a string to
+  see what it does!
 
-    New:
-    Write a function `alternatingLetters` that takes in a string and returns a
-    version of that string where letters alternate uppercase and lowercase,
-    starting with lowercase. Do not make exceptions for spaces within the
-    string.
+  New:
+  Write a function `alternatingLetters` that takes in a string and returns a
+  version of that string where letters alternate uppercase and lowercase,
+  starting with lowercase. Do not make exceptions for spaces within the
+  string.
 
-    Strings are immutable, so here are some tools you may find useful. The
-    `.split('')` function on strings to make a copy of the string as an array.
-    The `.join('')` function joins the elements in an array into a string.
+  Strings are immutable, so here are some tools you may find useful. The
+  `.split('')` function on strings to make a copy of the string as an array.
+  The `.join('')` function joins the elements in an array into a string.
 */
 function whisper(str) {
   return str.toLowerCase();
 }
 
 const alternatingLetters = (str) => {
-  let letter = str.split("");
-
-  for (let index = 0; index < letter.length; index++) {
-    if (index % 2 === 0) {
-      letter[index] = letter[index].toLowerCase();
+  // 0 - small
+  // 1- big
+  // turn strings into array
+  let alternate = "";
+  for(let i = 0; i <  str.length; i++){
+    if(i % 2 === 0){
+      alternate += str[i].toLowerCase()
     } else {
-      letter[index] = letter[index].toUpperCase();
+      alternate += str[i].toUpperCase()
     }
   }
-  return letter.join("");
+  return alternate
 };
-
-console.log(alternatingLetters("Hello"));
 
 module.exports = {
   plusTen,
